fd271a36e40c67b9bda44f081892425ffc20b75e
Simon Young <simon.young83@me.com>
2020-09-09 18:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ function increment(n) {return (++n) }
 function decrement(n) {return (--n) }
 
 function makeInt (n) {return parseInt (n,10) }
+
+function preserveDecimal (n) {return parseFloat (n)} 
