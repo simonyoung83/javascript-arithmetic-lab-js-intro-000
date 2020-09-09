cd6b4410703afe8f752b0666841b1a96f204cbcb
Simon Young <simon.young83@me.com>
2020-09-09 16:56:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add () {}
 function add (a,b) {return (a+b)}
 function multiply (a,b) {return (a*b) }
-function devide (a,b) {return (a/b) }
+function divide (a,b) {return (a/b) }
 function subtract (a,b) {return (a-b) }
