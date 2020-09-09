6a4e0ece67b98c41a59ad74db17e0d16eba56e95
Simon Young <simon.young83@me.com>
2020-09-09 17:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function multiply (a,b) {return (a*b) }
 function divide (a,b) {return (a/b) }
 function subtract (a,b) {return (a-b) }
 
-function increment(n) {return (n++)}
+function increment(n) {return n++}
