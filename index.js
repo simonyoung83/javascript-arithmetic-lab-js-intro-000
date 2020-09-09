986b22f1ba81249c8b2c55576aa0039204c7404f
Simon Young <simon.young83@me.com>
2020-09-09 17:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ function add (a,b) {return (a+b)}
 function multiply (a,b) {return (a*b) }
 function divide (a,b) {return (a/b) }
 function subtract (a,b) {return (a-b) }
+
+var number = 5 {return number ++}
